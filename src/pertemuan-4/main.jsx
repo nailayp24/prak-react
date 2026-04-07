d34b8fa58283233d4d29abe97256c6481214b1ec
nailayp24 <naila24si@mahasiswa.pcr.ac.id>
2026-04-07 16:26:05 +0700

    responsive

--- src/pertemuan-4/main.jsx
+++ src/pertemuan-4/main.jsx
@@ -2,12 +2,15 @@ import { createRoot } from "react-dom/client";
 import './tailwind.css';
 import FrameworkList from "./FrameworkList";
 import FrameworkListSearchFilter from "./FrameworkListSearchFilter";
+import ResponsiveText from "./ResponsiveDesign";
 
 
 createRoot(document.getElementById("root"))
     .render(
         <div>
-           <FrameworkListSearchFilter />
+            <ResponsiveText/>
+            
+           {/* <FrameworkListSearchFilter /> */}
           
         
         </div>
